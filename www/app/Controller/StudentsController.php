3e6cda902f95fc89d03720d329a565383c677c2e
Strelov Anton <DDoSed@mail.ru>
2012-07-31 12:58:12 +0400

    попытался сделать выборку с JOIN не вышло

--- www/app/Controller/StudentsController.php
+++ www/app/Controller/StudentsController.php
@@ -1,8 +1,20 @@
 <?php
    class StudentsController extends AppController{
 
+   	  /* $options['joins'] = array(
+   	   	   array('table' => 'people',
+   	   	   	'alias' => 'Person',
+   	   	   	'type' => 'LEFT',
+   	   	   	'conditions' => array(
+   	   	   		'Person.id = Student.person_id',
+   	   	   		)
+
+   	   	   )
+   	    );*/
+
+
    	   function index(){
-   	   	  $this->set('students',$this->Student->find('all'));
+   	   	  $this->set('students',$this->Student->find('all'/*,$options*/));
    	   }
    }
 ?>
