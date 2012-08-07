a3150e307b792e4da2c816f8fbdec5bc1d4314e1
Strelov Anton <DDoSed@mail.ru>
2012-08-07 20:05:04 +0400

    валидация данных для модели

--- www/app/Model/Group.php
+++ www/app/Model/Group.php
@@ -10,6 +10,52 @@ class Group extends AppModel {
 	);
 	public $hasMany = 'GroupStudent';
 	public $displayField = 'number';
+	public $validation = array(
+		'number' => array(
+			'notEmpty' =>array(
+				'rule' => 'notEmpty',
+				'message' => 'Номер группы не может быть пустым'
+			),
+			'numeric' =>array(
+				'rule' => 'numeric',
+				'message' => 'Номер группы должен быть числом'
+			)
+		),
+
+		'education_form' => array(
+			'notEmpty' =>array(
+				'rule' => 'notEmpty',
+				'message' => 'Выберете форму образования'
+			),
+			'numeric' =>array(
+				'rule' => 'numeric',
+				'message' => 'Выберете существующую форму образования'
+			)
+		),
+
+		'entrant_year' => array(
+			'notEmpty' =>array(
+				'rule' => 'notEmpty',
+				'message' => 'Выберете дату'
+			),
+			'date' =>array(
+				'rule' => 'date',
+				'message' => 'Введите корректную дату дату'
+			)
+		),
+
+		'speciality_id' => array(
+			'notEmpty' =>array(
+				'rule' => 'notEmpty',
+				'message' => 'Выберете специальность'
+			),
+			'numeric' =>array(
+				'rule' => 'numeric',
+				'message' => 'Выберете корректную специальность'
+			)
+		)
+
+	);
 
 	public function index($faculty_id, $course){
 		$options['joins'] = array(
